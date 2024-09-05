0d6aae9b02bd92cd1226671b13697fbc02440036
lizardN <blutech02@gmail.com>
2024-09-05 12:03:25 +0200

    local

--- routes/index.js
+++ routes/index.js
@@ -6196,7 +6196,7 @@ router.get('/grvFileV/:id',function(req,res){
       method: "POST",
      //url: 'https://portal.steuritinternationalschool.org/clerk/uploadStatement',
        //url: 'https://niyonsoft.org/uploadStatementDispatch',
-       url:'http:///niyonsoft.org/8000/uploadGrv',
+       url:'http:///niyonsoft.org/uploadGrv',
       headers: {
         "Content-Type": "multipart/form-data"  
       },
