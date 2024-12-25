2e29c55ad17ae404e502eea48048a406872b173f
lizardN <blutech02@gmail.com>
2024-12-25 09:05:12 +0200

    20

--- routes/receiver.js
+++ routes/receiver.js
@@ -1321,7 +1321,7 @@ let cases = docs.length
    
     var productChunks = [];
     var chunkSize = 140;
-    for (var i = 10; i < docs.length; i += chunkSize) {
+    for (var i = 20; i < docs.length; i += chunkSize) {
         productChunks.push(docs.slice(i, i + chunkSize));
     }
 
