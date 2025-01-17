2eb76ed3bf69891188fdfb485dd416dd6aa1762d
lizardN <blutech02@gmail.com>
2025-01-17 16:04:17 +0200

    productionReports

--- routes/rm.js
+++ routes/rm.js
@@ -4249,6 +4249,468 @@ router.get('/closeBlending',isLoggedIn,function(req,res){
 
 
 
+      router.get('/statementGenGW/',isLoggedIn,function(req,res){
+        //console.log(arrStatementR,'arrSingleUpdate')
+        var arrStatemementR=[]
+        var m = moment()
+        var mformat = m.format('L')
+        var month = m.format('MMMM')
+        var year = m.format('YYYY')
+        var date = req.user.date
+        //var receiveDate = req.user.dispatchDate
+        //var code ="Tiana Madzima"
+       
+        var arrG = []
+        BatchGingerWash.find().lean().then(docs=>{
+  
+  
+        
+     
+        //arrG.push(docs)
+          
+          console.log(docs,'arrG')
+        //var studentName = 'Tiana Madzima'
+        
+        /*console.log(arr,'iiii')*/
+        
+        RefNoSeq.find(function(err,doc){
+          let seqNum = doc[0].num
+          let seqId = doc[0]._id
+        //console.log(docs,'docs')
+        
+        const compile = async function (templateName, docs){
+        const filePath = path.join(process.cwd(),'templates',`${templateName}.hbs`)
+        
+        const html = await fs.readFile(filePath, 'utf8')
+        
+        return hbs.compile(html)(docs)
+        
+        };
+        
+        
+        
+        
+        (async function(){
+        
+        try{
+        //const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+        ],
+        executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+        });
+        
+        const page = await browser.newPage()
+        
+        
+        
+        //const content = await compile('report3',arr[uid])
+        const content = await compile('gw',docs)
+        
+        //const content = await compile('index',arr[code])
+        
+        await page.setContent(content, { waitUntil: 'networkidle2'});
+        //await page.setContent(content)
+        //create a pdf document
+        await page.emulateMediaType('screen')
+        //let height = await page.evaluate(() => document.documentElement.offsetHeight);
+        await page.evaluate(() => matchMedia('screen').matches);
+        await page.setContent(content, { waitUntil: 'networkidle0'});
+        //console.log(await page.pdf(),'7777')
+         
+        let filename = 'GW'+seqNum+'.pdf'
+        await page.pdf({
+        //path:('../gitzoid2/reports/'+year+'/'+month+'/'+uid+'.pdf'),
+        path:(`./public/statements/${year}/${month}/GW${seqNum}`+'.pdf'),
+        format:"A4",
+        width:'30cm',
+        height:'21cm',
+        //height: height + 'px',
+        printBackground:true
+        
+        })
+        
+        res.redirect('/rm/fileIdGW/'+filename)
+        
+        var repo = new RepoFiles();
+        
+        repo.filename = filename;
+        repo.fileId = "null";
+        repo.status = 'GW'
+        repo.date = mformat
+        repo.year = year;
+        repo.month = month
+        
+        
+        console.log('done')
+        
+        repo.save().then(poll =>{
+        
+        })
+        
+        
+        //upload.single('3400_Blessing_Musasa.pdf')
+        
+        
+        
+        /*await browser.close()
+        
+        /*process.exit()*/
+        
+        const file = await fs.readFile(`./public/statements/${year}/${month}/GW${seqNum}`+'.pdf');
+        const form = new FormData();
+        form.append("file", file,filename);
+        //const headers = form.getHeaders();
+        //Axios.defaults.headers.cookie = cookies;
+        //console.log(form)
+        await Axios({
+          method: "POST",
+         //url: 'https://portal.steuritinternationalschool.org/clerk/uploadStatement',
+       url: 'https://niyonsoft.org/rm/uploadStatementGW',
+          //url:'http://localhost:8000/rm/uploadStatementGW',
+          headers: {
+            "Content-Type": "multipart/form-data"  
+          },
+          data: form
+        });
+        
+        seqNum++
+        RefNoSeq.findByIdAndUpdate(seqId,{$set:{num:seqNum}},function(err,tocs){
+        
+        })
+          
+        
+       // res.redirect('/receiver/fileId/'+filename);
+        
+        
+        }catch(e) {
+        
+        console.log(e)
+        
+        
+        }
+        
+        
+        }) ()
+        
+        })
+      })
+        
+        //res.redirect('/hostel/discList')
+        
+        })
+        
+  
+        
+  router.get('/openFileGW/:id',isLoggedIn,function(req,res){
+    var filename = req.params.id
+    var batchNumber = req.user.batchNumber
+    var m = moment()
+    var mformat = m.format('L')
+    var month = m.format('MMMM')
+    var year = m.format('YYYY')
+    const path =`./public/statements/${year}/${month}/${filename}`+'.pdf'
+    if (fs.existsSync(path)) {
+        res.contentType("application/pdf");
+        fs.createReadStream(path).pipe(res)
+    } else {
+        res.status(500)
+        console.log('File not found')
+        res.send('File not found')
+    }
+    
+    })
+    
+        
+  router.post('/uploadStatementGW',upload.single('file'),(req,res,nxt)=>{
+    var fileId = req.file.id
+    console.log(fileId,'fileId')
+    var filename = req.file.filename
+    console.log(filename,'filename')
+  RepoFiles.find({filename:filename},function(err,docs){
+  if(docs.length>0){
+  
+  
+  //console.log(docs,'docs')
+  let id = docs[0]._id
+  RepoFiles.findByIdAndUpdate(id,{$set:{fileId:fileId}},function(err,tocs){
+  
+  })
+  
+  }
+  //res.redirect('/receiver/fileId/'+filename)
+  res.redirect('/rm/fileIdGW/'+filename)
+  })
+  
+  })
+  
+  
+  router.get('/fileIdGW/:id',function(req,res){
+    console.log(req.params.id)
+    var id = req.params.id
+    
+    res.redirect('/rm/openGW/'+id)
+    
+    })
+  
+  
+  router.get('/openGW/:id',(req,res)=>{
+    var filename = req.params.id
+    console.log(filename,'fileId')
+      const bucket = new mongodb.GridFSBucket(conn.db,{ bucketName: 'uploads' });
+      gfs.files.find({filename: filename}).toArray((err, files) => {
+      console.log(files[0])
+    
+        const readStream = bucket.openDownloadStream(files[0]._id);
+            readStream.pipe(res);
+    
+      })
+     //gfs.openDownloadStream(ObjectId(mongodb.ObjectId(fileId))).pipe(fs.createWriteStream('./outputFile'));
+    })
+  
+  
+  ///gingerCrushing
+
+  router.get('/statementGenGC/',isLoggedIn,function(req,res){
+    //console.log(arrStatementR,'arrSingleUpdate')
+    var arrStatemementR=[]
+    var m = moment()
+    var mformat = m.format('L')
+    var month = m.format('MMMM')
+    var year = m.format('YYYY')
+    var date = req.user.date
+    //var receiveDate = req.user.dispatchDate
+    //var code ="Tiana Madzima"
+   
+    var arrG = []
+    BatchGingerCrush.find().lean().then(docs=>{
+
+
+    
+ 
+    //arrG.push(docs)
+      
+      console.log(docs,'arrG')
+    //var studentName = 'Tiana Madzima'
+    
+    /*console.log(arr,'iiii')*/
+    
+    RefNoSeq.find(function(err,doc){
+      let seqNum = doc[0].num
+      let seqId = doc[0]._id
+    //console.log(docs,'docs')
+    
+    const compile = async function (templateName, docs){
+    const filePath = path.join(process.cwd(),'templates',`${templateName}.hbs`)
+    
+    const html = await fs.readFile(filePath, 'utf8')
+    
+    return hbs.compile(html)(docs)
+    
+    };
+    
+    
+    
+    
+    (async function(){
+    
+    try{
+    //const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+    "--disable-setuid-sandbox",
+    "--no-sandbox",
+    "--single-process",
+    "--no-zygote",
+    ],
+    executablePath:
+    process.env.NODE_ENV === "production"
+      ? process.env.PUPPETEER_EXECUTABLE_PATH
+      : puppeteer.executablePath(),
+    });
+    
+    const page = await browser.newPage()
+    
+    
+    
+    //const content = await compile('report3',arr[uid])
+    const content = await compile('gc',docs)
+    
+    //const content = await compile('index',arr[code])
+    
+    await page.setContent(content, { waitUntil: 'networkidle2'});
+    //await page.setContent(content)
+    //create a pdf document
+    await page.emulateMediaType('screen')
+    //let height = await page.evaluate(() => document.documentElement.offsetHeight);
+    await page.evaluate(() => matchMedia('screen').matches);
+    await page.setContent(content, { waitUntil: 'networkidle0'});
+    //console.log(await page.pdf(),'7777')
+     
+    let filename = 'GC'+seqNum+'.pdf'
+    await page.pdf({
+    //path:('../gitzoid2/reports/'+year+'/'+month+'/'+uid+'.pdf'),
+    path:(`./public/statements/${year}/${month}/GC${seqNum}`+'.pdf'),
+    format:"A4",
+    width:'30cm',
+    height:'21cm',
+    //height: height + 'px',
+    landscape: true,
+    printBackground:true
+    
+    })
+    
+    res.redirect('/rm/fileIdGC/'+filename)
+    
+    var repo = new RepoFiles();
+    
+    repo.filename = filename;
+    repo.fileId = "null";
+    repo.status = 'GC'
+    repo.date = mformat
+    repo.year = year;
+    repo.month = month
+    
+    
+    console.log('done')
+    
+    repo.save().then(poll =>{
+    
+    })
+    
+    
+    //upload.single('3400_Blessing_Musasa.pdf')
+    
+    
+    
+    /*await browser.close()
+    
+    /*process.exit()*/
+    
+    const file = await fs.readFile(`./public/statements/${year}/${month}/GC${seqNum}`+'.pdf');
+    const form = new FormData();
+    form.append("file", file,filename);
+    //const headers = form.getHeaders();
+    //Axios.defaults.headers.cookie = cookies;
+    //console.log(form)
+    await Axios({
+      method: "POST",
+     //url: 'https://portal.steuritinternationalschool.org/clerk/uploadStatement',
+   url: 'https://niyonsoft.org/rm/uploadStatementGC',
+      //url:'http://localhost:8000/rm/uploadStatementGC',
+      headers: {
+        "Content-Type": "multipart/form-data"  
+      },
+      data: form
+    });
+    
+    seqNum++
+    RefNoSeq.findByIdAndUpdate(seqId,{$set:{num:seqNum}},function(err,tocs){
+    
+    })
+      
+    
+   // res.redirect('/receiver/fileId/'+filename);
+    
+    
+    }catch(e) {
+    
+    console.log(e)
+    
+    
+    }
+    
+    
+    }) ()
+    
+    })
+  })
+    
+    //res.redirect('/hostel/discList')
+    
+    })
+    
+
+    
+router.get('/openFileGC/:id',isLoggedIn,function(req,res){
+var filename = req.params.id
+var batchNumber = req.user.batchNumber
+var m = moment()
+var mformat = m.format('L')
+var month = m.format('MMMM')
+var year = m.format('YYYY')
+const path =`./public/statements/${year}/${month}/${filename}`+'.pdf'
+if (fs.existsSync(path)) {
+    res.contentType("application/pdf");
+    fs.createReadStream(path).pipe(res)
+} else {
+    res.status(500)
+    console.log('File not found')
+    res.send('File not found')
+}
+
+})
+
+    
+router.post('/uploadStatementGC',upload.single('file'),(req,res,nxt)=>{
+var fileId = req.file.id
+console.log(fileId,'fileId')
+var filename = req.file.filename
+console.log(filename,'filename')
+RepoFiles.find({filename:filename},function(err,docs){
+if(docs.length>0){
+
+
+//console.log(docs,'docs')
+let id = docs[0]._id
+RepoFiles.findByIdAndUpdate(id,{$set:{fileId:fileId}},function(err,tocs){
+
+})
+
+}
+//res.redirect('/receiver/fileId/'+filename)
+res.redirect('/rm/fileIdGC/'+filename)
+})
+
+})
+
+
+router.get('/fileIdGC/:id',function(req,res){
+console.log(req.params.id)
+var id = req.params.id
+
+res.redirect('/rm/openGC/'+id)
+
+})
+
+
+router.get('/openGC/:id',(req,res)=>{
+var filename = req.params.id
+console.log(filename,'fileId')
+  const bucket = new mongodb.GridFSBucket(conn.db,{ bucketName: 'uploads' });
+  gfs.files.find({filename: filename}).toArray((err, files) => {
+  console.log(files[0])
+
+    const readStream = bucket.openDownloadStream(files[0]._id);
+        readStream.pipe(res);
+
+  })
+ //gfs.openDownloadStream(ObjectId(mongodb.ObjectId(fileId))).pipe(fs.createWriteStream('./outputFile'));
+})
+
+  
+  
+  
+
 
 
 
